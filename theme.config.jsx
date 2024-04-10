9723f7a9e50356380ba52fd2530df31422bc0b77
Agniva <dev@boringmarketing.com>
2024-04-10 18:30:07 +0530

    comit

---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Decentralized Finance (DeFi) Course Documentation | Web3 Ethiopia</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/Web3-Ethiopia/Web3-Class-Code",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/mragniva" target="_blank">
+              Agniva & Rishabh - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Decentralized Finance (DeFi) Course Documentation" />
+          <meta property="og:description" content="This Content is of a class where the instructor is teaching concepts mentioned in the transcript" />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
